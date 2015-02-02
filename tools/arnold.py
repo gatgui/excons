32796d74f6bed70c26822ace32948bcaa26cdfcf
gatgui <gatgui@gmail.com>
2015-02-02 11:43:09 +0900

    added 'Version' function to arnold extension

--- tools/arnold.py
+++ tools/arnold.py
@@ -20,6 +20,8 @@
 from SCons.Script import *
 import excons
 import sys
+import re
+import os
 
 def PluginExt():
   if str(Platform()) == "darwin":
@@ -28,6 +30,35 @@ def PluginExt():
     return ".dll"
   else:
     return ".so"
+
+def Version(asString=True):
+  arnoldinc, _ = excons.GetDirs("arnold", libdirname=("bin" if sys.platform != "win32" else "lib"))
+  
+  ai_version = os.path.join(arnoldinc, "ai_version.h")
+  
+  varch, vmaj, vmin, vpatch = 0, 0, 0, 0
+  
+  if os.path.isfile(ai_version):
+    defexp = re.compile(r"^\s*#define\s+AI_VERSION_(ARCH_NUM|MAJOR_NUM|MINOR_NUM|FIX)\s+([^\s]+)")
+    f = open(ai_version, "r")
+    for line in f.readlines():
+      m = defexp.match(line)
+      if m:
+        which = m.group(1)
+        if which == "ARCH_NUM":
+          varch = int(m.group(2))
+        elif which == "MAJOR_NUM":
+          vmaj = int(m.group(2))
+        elif which == "MINOR_NUM":
+          vmin = int(m.group(2))
+        elif which == "FIX":
+          m = re.search(r"\d+", m.group(2))
+          vpatch = (0 if m is None else int(m.group(0)))
+    f.close()
+  
+  rv = (varch, vmaj, vmin, vpatch)
+
+  return ("%s.%s.%s.%s" % rv if asString else rv)
 
 def Require(env):
   arnoldinc, arnoldlib = excons.GetDirs("arnold", libdirname=("bin" if sys.platform != "win32" else "lib"))
