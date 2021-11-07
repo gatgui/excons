bc5f37fdea973e3ca5456c8cd3a986b0bf1ec49b
Gaetan Guidet <gatgui@gmail.com>
2021-11-07 11:30:26 +0900

    first pass py3 supportfor envext submodules

--- envext/cmake.py
+++ envext/cmake.py
@@ -24,107 +24,110 @@
 
 
 import os
+import io
 import excons
 import pprint
 import excons.cmake as cmake
 import SCons.Script # pylint: disable=import-error
 
-# pylint: disable=bad-indentation,deprecated-lambda,unused-argument,bare-except,eval-used
+# pylint: disable=unused-argument
 
 
 def DummyScanner(node, env, path):
-   return []
+    return []
 
 def ConfigureAction(target, source, env):
-   if not cmake.Configure(env["CMAKE_PROJECT"], topdir=env["CMAKE_TOPDIR"], opts=env["CMAKE_OPTIONS"], flags=env["CMAKE_FLAGS"], min_mscver=env["CMAKE_MIN_MSCVER"]):
-      if os.path.isfile(env["CMAKE_CONFIG_CACHE"]):
-         os.remove(env["CMAKE_CONFIG_CACHE"])
-      if os.path.isfile(env["CMAKE_CACHE"]):
-         os.remove(env["CMAKE_CACHE"])
-      raise Exception("CMake Configure Failed")
-   return None
+    if not cmake.Configure(env["CMAKE_PROJECT"], topdir=env["CMAKE_TOPDIR"], opts=env["CMAKE_OPTIONS"], flags=env["CMAKE_FLAGS"], min_mscver=env["CMAKE_MIN_MSCVER"]):
+        if os.path.isfile(env["CMAKE_CONFIG_CACHE"]):
+            os.remove(env["CMAKE_CONFIG_CACHE"])
+        if os.path.isfile(env["CMAKE_CACHE"]):
+            os.remove(env["CMAKE_CACHE"])
+        raise Exception("CMake Configure Failed")
+    return None
 
 def BuildAction(target, source, env):
-   if not cmake.Build(env["CMAKE_PROJECT"], config=env["CMAKE_CONFIG"], target=env["CMAKE_TARGET"]):
-      raise Exception("CMake Build Failed")
-   return None
+    if not cmake.Build(env["CMAKE_PROJECT"], config=env["CMAKE_CONFIG"], target=env["CMAKE_TARGET"]):
+        raise Exception("CMake Build Failed")
+    return None
 
 def SetupEnvironment(env, settings):
-   name = settings["name"]
+    name = settings["name"]
 
-   debug = (excons.GetArgument("debug", 0, int) != 0)
-   opts = settings.get("cmake-opts", {})
-   flags = settings.get("cmake-flags", "")
-   blddir = cmake.BuildDir(name)
-   cmakec = blddir + "/CMakeCache.txt"
-   cfgc = cmake.ConfigCachePath(name)
-   cexts = [".c", ".h", ".cc", ".hh", ".cpp", ".hpp", ".cxx", ".hxx"]
+    debug = (excons.GetArgument("debug", 0, int) != 0)
+    opts = settings.get("cmake-opts", {})
+    flags = settings.get("cmake-flags", "")
+    blddir = cmake.BuildDir(name)
+    cmakec = blddir + "/CMakeCache.txt"
+    cfgc = cmake.ConfigCachePath(name)
+    cexts = [".c", ".h", ".cc", ".hh", ".cpp", ".hpp", ".cxx", ".hxx"]
 
-   # Override default C/C++ file scanner to avoid SCons being too nosy
-   env.Prepend(SCANNERS=SCons.Script.Scanner(function=DummyScanner, skeys=cexts))
-   env["CMAKE_PROJECT"] = name
-   env["CMAKE_TOPDIR"] = excons.abspath(settings.get("cmake-root", "."))
-   env["CMAKE_OPTIONS"] = opts
-   env["CMAKE_FLAGS"] = flags
-   env["CMAKE_MIN_MSCVER"] = settings.get("cmake-min-mscver", None)
-   env["CMAKE_CONFIG"] = settings.get("cmake-config", ("debug" if debug else "release"))
-   env["CMAKE_TARGET"] = settings.get("cmake-target", "install")
-   env["CMAKE_CACHE"] = cmakec
-   env["CMAKE_CONFIG_CACHE"] = cfgc
-   env["BUILDERS"]["CMakeConfigure"] = SCons.Script.Builder(action=SCons.Script.Action(ConfigureAction, "Configure using CMake ..."))
-   env["BUILDERS"]["CMake"] = SCons.Script.Builder(action=SCons.Script.Action(BuildAction, "Build using CMake ..."))
+    # Override default C/C++ file scanner to avoid SCons being too nosy
+    env.Prepend(SCANNERS=SCons.Script.Scanner(function=DummyScanner, skeys=cexts))
+    env["CMAKE_PROJECT"] = name
+    env["CMAKE_TOPDIR"] = excons.abspath(settings.get("cmake-root", "."))
+    env["CMAKE_OPTIONS"] = opts
+    env["CMAKE_FLAGS"] = flags
+    env["CMAKE_MIN_MSCVER"] = settings.get("cmake-min-mscver", None)
+    env["CMAKE_CONFIG"] = settings.get("cmake-config", ("debug" if debug else "release"))
+    env["CMAKE_TARGET"] = settings.get("cmake-target", "install")
+    env["CMAKE_CACHE"] = cmakec
+    env["CMAKE_CONFIG_CACHE"] = cfgc
+    env["BUILDERS"]["CMakeConfigure"] = SCons.Script.Builder(action=SCons.Script.Action(ConfigureAction, "Configure using CMake ..."))
+    env["BUILDERS"]["CMake"] = SCons.Script.Builder(action=SCons.Script.Action(BuildAction, "Build using CMake ..."))
 
-   # Check if we need to reconfigure
-   if not SCons.Script.GetOption("clean"):
-      if not os.path.isdir(blddir):
-         try:
-            os.makedirs(blddir)
-         except:
-            return None
-
-      doconf = True
-      if os.path.isfile(cfgc):
-         doconf = False
-         with open(cfgc, "r") as f:
+    # Check if we need to reconfigure
+    if not SCons.Script.GetOption("clean"):
+        if not os.path.isdir(blddir):
             try:
-               d = eval(f.read())
-               for k, v in d.iteritems():
-                  if not k in opts or opts[k] != v:
-                     doconf = True
-                     break
-               if not doconf:
-                  for k, v in opts.iteritems():
-                     if not k in d:
-                        doconf = True
-                        break
-            except:
-               doconf = True
-      if doconf or int(SCons.Script.ARGUMENTS.get("reconfigure", "0")) != 0:
-         # Only rewrite cfgc when strictly needed
-         if doconf:
-            with open(cfgc, "w") as f:
-               pprint.pprint(opts, stream=f)
-         if os.path.isfile(cmakec):
-            os.remove(cmakec)
+                os.makedirs(blddir)
+            except Exception as e: # pylint: disable=broad-except
+                excons.WarnOnce("[cmake] Couldn't create directory: %s (%s)" % (blddir, e))
+                return None
 
-   cins = settings.get("cmake-cfgs", [])
-   cins.append(cfgc)
-   cins.extend(cmake.AdditionalConfigureDependencies(name))
-   cout = [cmakec]
+        doconf = True
+        if os.path.isfile(cfgc):
+            doconf = False
+            with io.open(cfgc, "r", encoding="UTF-8", newline="\n") as f:
+                try:
+                    d = eval(f.read()) # pylint: disable=eval-used
+                    for k, v in d.iteritems():
+                        if not k in opts or opts[k] != v:
+                            doconf = True
+                            break
+                    if not doconf:
+                        for k, v in opts.iteritems():
+                            if not k in d:
+                                doconf = True
+                                break
+                except Exception as e: # pylint: disable=broad-except
+                    excons.WarnOnce("[cmake] Failed to read configuration file: %s (%s)" % (cfgc, e))
+                    doconf = True
+        if doconf or int(SCons.Script.ARGUMENTS.get("reconfigure", "0")) != 0:
+            # Only rewrite cfgc when strictly needed
+            if doconf:
+                with io.open(cfgc, "w", encoding="UTF-8", newline="\n") as f:
+                    pprint.pprint(opts, stream=f)
+            if os.path.isfile(cmakec):
+                os.remove(cmakec)
 
-   env.CMakeConfigure(cout, cins)
+    cins = settings.get("cmake-cfgs", [])
+    cins.append(cfgc)
+    cins.extend(cmake.AdditionalConfigureDependencies(name))
+    cout = [cmakec]
 
-   bins = settings.get("cmake-srcs", [])
-   bins.extend(cout)
+    env.CMakeConfigure(cout, cins)
 
-   expected_outputs = settings.get("cmake-outputs", [])
-   expected_outputs = map(lambda x: (x if os.path.isabs(x) else (excons.OutputBaseDirectory() + "/" + x)), expected_outputs)
-   actual_outputs = cmake.Outputs(name)
-   bout = list(set(actual_outputs).union(set(expected_outputs))) + [cmake.OutputsCachePath(name)]
+    bins = settings.get("cmake-srcs", [])
+    bins.extend(cout)
 
-   out = env.CMake(bout, bins)
+    expected_outputs = settings.get("cmake-outputs", [])
+    expected_outputs = map(lambda x: (x if os.path.isabs(x) else (excons.OutputBaseDirectory() + "/" + x)), expected_outputs)
+    actual_outputs = cmake.Outputs(name)
+    bout = list(set(actual_outputs).union(set(expected_outputs))) + [cmake.OutputsCachePath(name)]
 
-   # Run clean last
-   cmake.CleanOne(name)
+    out = env.CMake(bout, bins)
 
-   return out
+    # Run clean last
+    cmake.CleanOne(name)
+
+    return out
